c5b0c3ae65bb693f54f83411abf595447f35665b
Scott Kaye <scott.kaye@meridiancu.ca>
2020-07-11 18:34:27 -0400

    First commit

---
+++ config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+	pathPrefix: "/photography", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+	siteTitle: "Scott Kaye", // Navigation and Site Title
+	siteTitleAlt: "Scott Kaye - Photography Portfolio", // Alternative Site title for SEO
+	siteTitleShort: "Scott Kaye", // short_name for manifest
+	siteHeadline: "Niagara-based hobby photographer", // Headline for schema.org JSONLD
+	siteUrl: "https://scott.kaye.family/photography", // Domain of your site. No trailing slash!
+	siteLanguage: "en", // Language Tag on <html> element
+	siteLogo: "/logos/logo.png", // Used for SEO and manifest
+	siteDescription: "Showcase of my photo portfolio.",
+	author: "Scott Kaye", // Author for schema.org JSONLD
+
+	// siteFBAppID: '123456789', // Facebook App ID - Optional
+	userTwitter: "", // Twitter Username
+	ogSiteName: "", // Facebook Site Name
+	ogLanguage: "en_US", // og:language
+	googleAnalyticsID: "UA-XXXXXX-X",
+
+	// Manifest and Progress color
+	themeColor: "#db7436",
+	backgroundColor: "#3b3c4f",
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import styled from "styled-components";
+import { animated, useSpring, config } from "react-spring";
+import Layout from "../components/layout";
+import GridItem from "../components/grid-item";
+import SEO from "../components/SEO";
+import { ChildImageSharp } from "../types";
+
+type PageProps = {
+	data: {
+		projects: {
+			nodes: {
+				title: string;
+				slug: string;
+				orientation: string;
+				cover: ChildImageSharp;
+			}[];
+		};
+	};
+};
+
+const Area = styled(animated.div)`
+	display: flex;
+	flex-wrap: wrap;
+
+	@media (max-width: ${(props) => props.theme.breakpoints[3]}) {
+	}
+
+	@media (max-width: ${(props) => props.theme.breakpoints[1]}) {
+	}
+
+	@media (max-width: ${(props) => props.theme.breakpoints[0]}) {
+	}
+`;
+
+const Project = styled(GridItem)`
+	flex: 1 1
+		${(props) => {
+			switch (props.orientation) {
+				default:
+					return `33%`;
+				case "landscape":
+					return `66%`;
+			}
+		}};
+	height: 70vh;
+`;
+
+const Index: React.FunctionComponent<PageProps> = ({ data: { projects } }) => {
+	const pageAnimation = useSpring({
+		config: config.slow,
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+	});
+
+	return (
+		<Layout>
+			<SEO />
+			<Area style={pageAnimation}>
+				{projects.nodes.map((project, i) => (
+					<Project
+						key={i}
+						to={project.slug}
+						orientation={project.orientation}
+						aria-label={`View project "${project.title}"`}
+					>
+						<Img fluid={project.cover.childImageSharp.fluid} />
+						<span>{project.title}</span>
+					</Project>
+				))}
+			</Area>
+		</Layout>
+	);
+};
+
+export default Index;
+
+export const query = graphql`
+	query Index {
+		projects: allProjectsYaml {
+			nodes {
+				title
+				slug
+				orientation
+				cover {
+					childImageSharp {
+						fluid(quality: 95, maxWidth: 1200) {
+							...GatsbyImageSharpFluid_withWebp
+						}
+					}
+				}
+			}
+		}
+	}
+`;
