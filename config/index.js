147847e228e6a27c425f2ef5578248f3d3b6fd03
Scott Kaye <scott.kaye@meridiancu.ca>
2020-07-11 20:35:58 -0400

    Firebase updates

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ module.exports = {
 	siteTitleAlt: "Scott Kaye - Photography Portfolio", // Alternative Site title for SEO
 	siteTitleShort: "Scott Kaye", // short_name for manifest
 	siteHeadline: "Niagara-based hobby photographer", // Headline for schema.org JSONLD
-	siteUrl: "https://scott.kaye.family/photography", // Domain of your site. No trailing slash!
+	siteUrl: "https://scottkaye.net/photography", // Domain of your site. No trailing slash!
 	siteLanguage: "en", // Language Tag on <html> element
 	siteLogo: "/logos/logo.png", // Used for SEO and manifest
 	siteDescription: "Showcase of my photo portfolio.",
