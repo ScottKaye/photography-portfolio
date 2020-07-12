b7f5fcb6794eb91089829cbd51bf6df36c5bc8ec
Scott Kaye <scott.kaye@meridiancu.ca>
2020-07-11 20:23:00 -0400

    Added a few images

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -229,7 +229,7 @@ const Layout = ({ children, color }: LayoutProps) => {
               justifyContent="space-between"
             >
               <Box width={['3rem', '4rem', '5rem', '6rem']}>
-                <Link to="/" aria-label="LekoArts, Back to Home">
+                <Link to="/" aria-label="Back to Home">
                   <Logo />
                 </Link>
               </Box>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import { config, useSpring } from 'react-spring'
-import Layout from '../components/layout'
-import { AnimatedBox } from '../elements'
-import SEO from '../components/SEO'
+import React from "react";
+import { config, useSpring } from "react-spring";
+import Layout from "../components/layout";
+import { AnimatedBox } from "../elements";
+import SEO from "../components/SEO";
 
 const About = () => {
-  const pageAnimation = useSpring({
-    config: config.slow,
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-  })
+	const pageAnimation = useSpring({
+		config: config.slow,
+		from: { opacity: 0 },
+		to: { opacity: 1 },
+	});
 
-  return (
-    <Layout>
-      <SEO title="About | Scott Kaye" desc="Hi. I'm LekoArts! You can visit my website or my other Gatsby projects." />
-      <AnimatedBox style={pageAnimation} py={[6, 6, 6, 8]} px={[6, 6, 8, 6, 8, 13]}>
-        <h1>Hi. I'm LekoArts!</h1>
-        <p>You can visit my website or my other Gatsby projects:</p>
-        <p>
-          <a href="https://www.lekoarts.de">Website</a> - <a href="https://themes.lekoarts.de">More projects</a>
-        </p>
-      </AnimatedBox>
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<SEO title="About | Scott Kaye" />
+			<AnimatedBox
+				style={pageAnimation}
+				py={[6, 6, 6, 8]}
+				px={[6, 6, 8, 6, 8, 13]}
+			>
+				<h1>&#x1f44b; Hi, I'm Scott!</h1>
+				<p>
+					I take photos for fun, and have a small but growing
+					collection of stock photographs on{" "}
+					<a
+						href="https://www.istockphoto.com/ca/portfolio/scottkaye"
+						target="_blank"
+						rel="noopener"
+					>
+						iStock.
+					</a>
+				</p>
+			</AnimatedBox>
+		</Layout>
+	);
+};
 
-export default About
+export default About;
